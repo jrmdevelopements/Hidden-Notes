8c4e0a0589f28e6b69487fbe3bd5a86c9184a7b7
jrmdevelopements <jrmdevelopements@gmail.com>
2026-03-01 20:25:05 +0530

    Testing full automation 5th

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use('/api/notes', noteRoutes);
 
 // Health check
 app.get('/', (req, res) => {
-  res.send('Job Notes API Running staging version try 4');
+  res.send('Job Notes API Running staging version try again');
 });
 
 const PORT = process.env.PORT || 5000;
